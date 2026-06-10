d023e32930beb28aa97a98e7ed3f04eb4be02b3e
Kevin <kevinrezende55@outlook.com>
2026-06-10 11:46:11 -0300

    fix: imagem caminho incorreto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import styles from "../styles/Header.module.css";
+import minhaImagem from "../assets/euAnimated.jpeg";
 
 function Header() {
   return (
     <header className={styles.headerContainer}>
       <h1 className={styles.nome}>Kevin Silva</h1>
       <p className={styles.profissao}>Desenvolvedor Full Stack</p>
-      <img
-        className={styles.imagem}
-        src="src/assets/euAnimated.jpeg"
-        alt="foto de perfil"
-      />
+
+      <img className={styles.imagem} src={minhaImagem} alt="foto de perfil" />
     </header>
   );
 }
